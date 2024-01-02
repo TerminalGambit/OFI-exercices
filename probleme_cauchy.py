88c435e7ff4b9955716c1749c5751bea6b91e398
TerminalGambit <jackmassey.monaco@outlook.com>
2024-01-02 15:41:57 +0100

    Refactor cauchy function to use memoization

--- probleme_cauchy.py
+++ probleme_cauchy.py
@@ -1,14 +1,19 @@
 # cn = cn-1 + cn-2 - cn-3
 # c0 = 0, c1 = 1, c2 = 1
 
-def cauchy(n):
-    if n == 0:
-        return 0
+def cauchy(n, memo={}):
+    if n in memo:
+        return memo[n]
+    elif n == 0:
+        result = 0
     elif n == 1:
-        return 1
+        result = 1
     elif n == 2:
-        return 1
+        result = 1
     else:
-        return cauchy(n-1) + cauchy(n-2) - cauchy(n-3)
+        result = cauchy(n-1, memo) + cauchy(n-2, memo) - cauchy(n-3, memo)
+    
+    memo[n] = result
+    return result
 
 print(cauchy(50), cauchy(73))
