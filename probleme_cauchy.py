4869348d12951f34afa972d00961f0cd9e082647
TerminalGambit <jackmassey.monaco@outlook.com>
2024-01-02 15:58:43 +0100

    Add cauchy2, cauchy3, and cauchy4 functions

--- probleme_cauchy.py
+++ probleme_cauchy.py
@@ -17,3 +17,78 @@ def cauchy(n, memo={}):
     return result
 
 print(cauchy(50), cauchy(73))
+
+# cn = cn-2 + n
+# c1 = 1, c0 = 0
+
+def cauchy2(n, memo={}):
+    if n in memo:
+        return memo[n]
+    elif n == 0:
+        result = 0
+    elif n == 1:
+        result = 1
+    else:
+        result = cauchy2(n-2, memo) + n
+    
+    memo[n] = result
+    return result
+
+print(cauchy2(50), cauchy2(73))
+
+# cn = cn-1 + n
+# c0 = 0
+
+def cauchy3(n, memo={}):
+    if n in memo:
+        return memo[n]
+    elif n == 0:
+        result = 0
+    else:
+        result = cauchy3(n-1, memo) + n
+    
+    memo[n] = result
+    return result
+
+# solution générale est p(n) où p est un polynôme de degré 2 ? vrai ou faux ?
+# p(n) = an² + bn + c
+# pourquoi ? car p(n) = p(n-1) + n
+# p(n-1) = a(n-1)² + b(n-1) + c
+# p(n) = a(n-1)² + b(n-1) + c + n
+
+# cn = 2ncn-1 + 1
+# c0 = 0
+
+def cauchy4(n, memo={}):
+    if n in memo:
+        return memo[n]
+    elif n == 0:
+        result = 0
+    else:
+        result = 2*n*cauchy4(n-1, memo) + 1
+    
+    memo[n] = result
+    return result
+
+def test_solution(f):
+    for n in range(10):
+        print(f(n))
+
+def cas_n(n):
+    return 2**n * factorial(n) + (-1)**n
+
+def cas_n2(n):
+    return 2**n + (-1)**(n)
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+print("Test cas_n:")
+test_solution(cas_n)
+print("\nTest cas_n2:")
+test_solution(cas_n2)
+print("\nTest cauchy4:")
+test_solution(cauchy4)
